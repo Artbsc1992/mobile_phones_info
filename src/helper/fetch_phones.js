a2a73cf128953471ccfcc4ec615f20b9bc647874
arturo <ahpareja@gmail.com>
2022-06-20 16:00:45 -0500

    Bug fix on fetch_phones

--- src/helper/fetch_phones.js
+++ src/helper/fetch_phones.js
@@ -1,15 +1,14 @@
 const fetchPhones = async () => {
   const result = await fetch ('https://api-mobilespecs.azharimm.site/v2/top-by-fans');
   const json = await result.json();
-  const phones = json.map((phone) =>({
-    id: new Date().getUTCMilliseconds(),
+  const phones = json.data.phones.map((phone) =>({
     name: phone.phone_name,
     slug: phone.slug,
     likes: phone.favorites,
     detail: phone.detail,
-  }));
-  console.log(phones)
+  }))
   return phones
+  
 };
 
 export default fetchPhones;
