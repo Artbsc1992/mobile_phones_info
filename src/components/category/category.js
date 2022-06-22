6a276ffe9e3625f8246366e42f8f0f2bc62d1e95
arturo <ahpareja@gmail.com>
2022-06-22 15:53:54 -0500

    Add loading page between articles calls

--- src/components/category/category.js
+++ src/components/category/category.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   getArticles, clean, showDescription, hideDescription,
 } from '../../redux/articles/articles';
+import LoadingPage from '../loadingPage/loadingPage';
 
 const Category = () => {
   const { categoryName } = useParams();
@@ -15,32 +16,42 @@ const Category = () => {
   }, []);
 
   return (
-    <div>
-      {categoryName.toUpperCase()}
-      {articles && articles.map((article) => (
-        <div key={article.title}>
-          <img alt="img" src={article.img} />
-          <h2>{article.price}</h2>
-          <h4>{article.title}</h4>
-          {!article.show
-            ? (
-              <button type="button" onClick={() => dispatch(showDescription(article.id))}>
-                Description
-              </button>
-            )
-            : (
-              <button type="button" onClick={() => dispatch(hideDescription(article.id))}>
-                Hide
-              </button>
-            )}
-          <p style={{ display: article.show ? 'block' : 'none' }}>{article.description}</p>
-          <span>{`${article.rate}/5`}</span>
-          {' '}
-          <span>{article.count}</span>
-        </div>
-      ))}
+    <>
+      {articles.length === 0
+        ? (
+          <div>
+            <LoadingPage />
+          </div>
+        )
+        : (
+          <div>
+            {categoryName.toUpperCase()}
+            {articles && articles.map((article) => (
+              <div key={article.title}>
+                <img alt="img" src={article.img} />
+                <h2>{article.price}</h2>
+                <h4>{article.title}</h4>
+                {!article.show
+                  ? (
+                    <button type="button" onClick={() => dispatch(showDescription(article.id))}>
+                      Description
+                    </button>
+                  )
+                  : (
+                    <button type="button" onClick={() => dispatch(hideDescription(article.id))}>
+                      Hide
+                    </button>
+                  )}
+                <p style={{ display: article.show ? 'block' : 'none' }}>{article.description}</p>
+                <span>{`${article.rate}/5`}</span>
+                {' '}
+                <span>{article.count}</span>
+              </div>
+            ))}
 
-    </div>
+          </div>
+        )}
+    </>
   );
 };
 export default Category;
---
+++ src/components/loadingPage/loadingPage.js
@@ -0,0 +1,16 @@
+import './loadingPage.css';
+
+const LoadingPage = () => (
+  <div className="lds-roller">
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default LoadingPage;
