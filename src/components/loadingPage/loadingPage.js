d4b08f14947751ebb64027423669e2d791a8b17d
arturo <ahpareja@gmail.com>
2022-06-22 15:55:55 -0500

    Add loading message

--- src/components/loadingPage/loadingPage.js
+++ src/components/loadingPage/loadingPage.js
@@ -1,16 +1,19 @@
 import './loadingPage.css';
 
 const LoadingPage = () => (
-  <div className="lds-roller">
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-    <div />
-  </div>
+  <>
+    <div className="lds-roller">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+    <div>Loading...</div>
+  </>
 );
 
 export default LoadingPage;
