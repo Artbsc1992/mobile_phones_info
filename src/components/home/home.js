66f8a5350f208a89a362cf93328cf78aa0b00a06
arturo <ahpareja@gmail.com>
2022-06-22 22:01:33 -0500

    add loading page to home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getCategories } from '../../redux/categories/categories';
+import LoadingPage from '../loadingPage/loadingPage';
 import home from '../../assets/home.jpg';
 import './home.css';
 
@@ -14,32 +15,35 @@ const HomeList = () => {
 
   return (
     <>
-      <div className="articles_container">
-        <img className="home-img" src={home} alt="home" />
-        <p className="presentation">
-          <h2>We are open!</h2>
-          We are an online store with the newest technologie
-          and electronics
-        </p>
-        <div className="categories">
-          {categories && categories.map(
-            (category) => (
-              <Link to={`/category/${category.name}`} key={category.name} className={category.class}>
-                <p>{category.name}</p>
-                <span>
-                  (
-                  {category.productsCount}
-                  )
-                </span>
-              </Link>
-            ),
-          )}
-        </div>
-        <footer className="footer">
-          <span>Project build by Arturo</span>
-        </footer>
-      </div>
-
+      {categories.length === 0
+        ? <LoadingPage />
+        : (
+          <div className="articles_container">
+            <img className="home-img" src={home} alt="home" />
+            <div className="presentation">
+              <h2>We are open!</h2>
+              We are PREMIUM STORE, we deliver the most
+              trending articles just for exclusive customers like you!
+            </div>
+            <div className="categories">
+              {categories && categories.map(
+                (category) => (
+                  <Link to={`/category/${category.name}`} key={category.name} className={category.class}>
+                    <p>{category.name}</p>
+                    <span>
+                      (
+                      {category.productsCount}
+                      )
+                    </span>
+                  </Link>
+                ),
+              )}
+            </div>
+            <footer className="footer">
+              <span>Project build by Arturo</span>
+            </footer>
+          </div>
+        )}
     </>
   );
 };
